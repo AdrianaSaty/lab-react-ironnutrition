5669b36735445d0faa8a93ef2e76c6c0346602bc
Adriana Saty Bertelli <Adria@Adria-2.local>
2019-10-30 21:05:58 -0300

    1 and 2 interaction

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
 class App extends Component {
+  constructor(props) {
+    super(props),
+      this.state = {
+        foods: foods,
+        showForm: false,
+      }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      <div>
+        <h1>IronFoods</h1>
+        {this.state.foods.map(e =>{ 
+          let { name, calories, image, quantity} = e;
+          return (
+          <FoodBox name={name} calories={calories} image={image} quantity={quantity}/>
+          )
+        }
+        )}
+    </div>
+  )
 }
+}
+
 
 export default App;
