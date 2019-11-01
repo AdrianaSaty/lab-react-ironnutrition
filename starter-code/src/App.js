da470ec99c283082cffd98bdec81735d60b67557
Adriana Saty Bertelli <Adria@Adria-2.local>
2019-10-31 22:16:40 -0300

    interaction 4 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,11 +4,12 @@ import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import Form from './components/Form';
-import Search from './components/Search'
+import Search from './components/Search';
 
 class App extends Component {
   state = {
-    foods: foods,
+    foods: foods.slice(0),
+    filteredFoods: foods.slice(0)
   }
 
   addFoodHandler = (food) => {
@@ -19,17 +20,27 @@ class App extends Component {
     })
   }
 
+  searchFood = (search) => {
+    let filteredFoods = foods.filter(food => food.name.toLowerCase().includes(search.toLowerCase()) );
+    
+    this.setState({
+      filteredFoods: filteredFoods
+    })
+
+  }
+
   render() {
     return (
       <div>
         <Form addFood={this.addFoodHandler}/>
+        <Search handleSearchFood={(search) => this.searchFood(search)}/>
 
-        {this.state.foods.map((e, idx) =>{ 
-          let { name, calories, image, quantity} = e;
-          return (
-          <FoodBox name={name} calories={calories} image={image} quantity={quantity} key={idx}/>
-          )
-        }
+        {this.state.filteredFoods.map((e, idx) =>{ 
+            let { name, calories, image, quantity} = e;
+            return (
+            <FoodBox name={name} calories={calories} image={image} quantity={quantity} key={idx}/>
+            )
+          }
         )}
     </div>
   )
