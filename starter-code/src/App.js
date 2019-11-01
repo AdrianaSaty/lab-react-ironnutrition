e49dfa650e0baf23e44bd2bd280b01d755a29b26
Adriana Saty Bertelli <Adria@Adria-2.local>
2019-10-31 21:14:38 -0300

    interaction 3 done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
+import Form from './components/Form';
+import Search from './components/Search'
 
 class App extends Component {
-  constructor(props) {
-    super(props),
-      this.state = {
-        foods: foods,
-        showForm: false,
-      }
+  state = {
+    foods: foods,
+  }
+
+  addFoodHandler = (food) => {
+    const foodsCopy = [...this.state.foods];
+    foodsCopy.push(food);
+    this.setState({
+      foods: foodsCopy,
+    })
   }
 
   render() {
     return (
       <div>
-        <h1>IronFoods</h1>
-        {this.state.foods.map(e =>{ 
+        <Form addFood={this.addFoodHandler}/>
+
+        {this.state.foods.map((e, idx) =>{ 
           let { name, calories, image, quantity} = e;
           return (
-          <FoodBox name={name} calories={calories} image={image} quantity={quantity}/>
+          <FoodBox name={name} calories={calories} image={image} quantity={quantity} key={idx}/>
           )
         }
         )}
